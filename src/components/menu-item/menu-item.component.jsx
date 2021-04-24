7cc2198d5670dd57d582b93ceda03ee4bb4fbde6
Alan Callaghan <alan.callaghan@serialaffinity.co.uk>
2021-04-25 00:09:16 +0100

    Worked with improving styling for menu-items - hover effect, transition and backgroundimage.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 export const MenuItem = ({title, imageUrl, size}) => (
-    <div 
-        className={`${size} menu-item`}
-        style={{
-        backgroundImage: `url(${imageUrl})`
-    }}>
-                <div className="content">
-                    <h1 className="title">{title}</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
+    <div className={`${size} menu-item`}>
+        
+        <div 
+            className="background-image"
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+            
+        <div 
+            className="content">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
 ); 
 
 export default MenuItem;
