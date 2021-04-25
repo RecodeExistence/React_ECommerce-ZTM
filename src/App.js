f46a95ba5720c030168c25f454dc5df8b11e585b
Alan Callaghan <alan.callaghan@serialaffinity.co.uk>
2021-04-25 03:00:24 +0100

    Added Routes to homepage and hats page.  Used match.url on menu-item to build path with linkUrl prop.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Route, Switch, Link} from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component'; 
 import './pages/homepage/homepage.styles.scss';
 
-function App() {
-  return  <Homepage />
+
+const HatsPage = ({match}) => {
+  console.log(match.url);
+  return (
+    <div>
+        <h1>Hats</h1>
+        <Link to="/">Home</Link>
+  </div>
+  )
 }
+function App() {
+    return (
+      <div>
+        <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/hats" component={HatsPage} />
+        </Switch>
+       </div>    
+    ) 
+  }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-export const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+export const MenuItem = ({title, imageUrl, size, linkUrl, history, match}) => (
+    <div 
+        className={`${size} menu-item`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
         
         <div 
             className="background-image"
             style={{
-                backgroundImage: `url(${imageUrl})`
+            backgroundImage: `url(${imageUrl})`
             }}
         />
-            
+
+    
         <div 
             className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
@@ -19,4 +24,4 @@ export const MenuItem = ({title, imageUrl, size}) => (
     </div>
 ); 
 
-export default MenuItem;
+export default withRouter(MenuItem);
