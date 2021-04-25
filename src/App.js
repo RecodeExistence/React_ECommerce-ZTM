4ea5ed8fa725bf8aa033b629843e443a0e31c8dc
Alan Callaghan <alan.callaghan@serialaffinity.co.uk>
2021-04-26 00:17:22 +0100

    Updated app with shop page, collection preview component, collection item. Added header and related styling.  Updated App.js to include header outwith switch so available on all component page loads. Added assets for logo.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Route, Switch} from 'react-router-dom';
 
-import './App.css';
+import Header from './components/header/header.component'; 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'; 
 
 
+import './App.css';
 import './pages/homepage/homepage.styles.scss';
 
 function App() {
     return (
-      <div>
+      <div className="App">
+      <Header />
         <Switch>
             <Route exact path="/" component={Homepage} />
             <Route path="/shop" component={ShopPage} />
