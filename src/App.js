165961b85c4e036692303cec1fdea94849507d14
Alan Callaghan <alan.callaghan@serialaffinity.co.uk>
2021-05-06 23:57:22 +0100

    continued work on FireBase Oauth.  Set up Sign-In / Sign out, and display of logged in user when that happens.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route, Switch} from 'react-router-dom';
+import { auth } from './firebase/firebase.utils';
 
 import Header from './components/header/header.component'; 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'; 
 
 
-import './App.css';
+import './App.scss';
 import './pages/homepage/homepage.styles.scss';
 
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      currentUser : null
+    }
+
+  }
+  
+  unsubscribeFromAuth = null; 
+  
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser : user})
+    });
+  }
+  
+  componentWillUnmount = () => this.unsubscribeFromAuth();
+
+  render(){
     return (
       <div className="App">
-      <Header />
+      {
+          this.state.currentUser ? 
+      <Fragment>
+           <div className="user-name-display">
+              <div className='username'>Logged In as {this.state.currentUser.displayName}</div> 
+          </div>
+    </Fragment>
+            : null
+        }
+      <Header userLoggedIn={this.state.currentUser} />
         <Switch>
             <Route exact path="/" component={Homepage} />
             <Route path="/shop" component={ShopPage} />
             <Route path="/sign-in-and-sign-up" component={SignInAndSignUpPage}/>
         </Switch>
-       </div>    
-    ) 
+       </div>
+    )
   }
+}
 
 export default App;
